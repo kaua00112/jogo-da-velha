cbfd4c058b6550db7eeb7e949e09d4f74b878b51
kaua00112 <kaua824@outlook.com>
2022-02-05 13:07:36 -0300

    primeiro projeto em react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  
+
+  const emptyBoard = Array(9).fill("");
+
+  const exemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  const [board, setBoard] = useState(emptyBoard);
+  const[currentPlayer, setCurrentPlayer] = useState("O");
+  const[winner, setWinner] = useState("");
+
+  const handleCellClick = (index: number) => {
+    if (winner){
+      return null;
+    }
+
+    if (board[index] !=="") {
+      return null;
+    }
+    
+    
+    setBoard(board.map((item, itemIndex) => itemIndex === 
+    index ? currentPlayer : item)
+    );
+
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X" );
+  }
+
+  const checkWinner = () => {
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+
+    ];
+
+    possibleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell === "O")) setWinner("O");
+      if (cells.every(cell => cell === "X")) setWinner("X");
+    });
+
+    checkDraw();
+
+
+  }
+
+  const checkDraw = () => {
+    if (board.every(item => item !== ""))setWinner("E");
+    
+  }
+
+  useEffect(checkWinner, [board]);
+
+  const resetGame = () => {
+    setCurrentPlayer("O");
+    setBoard(emptyBoard);
+    setWinner("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <main>
+    <h1 className="title">Jogo dos Véios</h1>
+
+    <div className={`board ${winner ? "game-over" : ""}`}>
+      {board.map((item, index) => (
+      <div
+        key={index}
+        className={`cell ${item}`}
+        onClick={() => handleCellClick(index)}
+       >
+         {item}
+         </div>
+      ))}
     </div>
+
+    {winner &&
+    <footer>
+      {winner === "E" ?
+        <h2 className='"winner-message'>
+        <span className={winner}>Empatou!</span> 
+        </h2>
+      :
+      
+        <h2 className='"winner-message'>
+        <span className={winner}>{winner}</span>  venceu!
+        </h2>
+      }
+      <button onClick={resetGame}>Recomeçar Jogo!</button>
+    </footer>
+    }
+    </main>
   );
 }
 
